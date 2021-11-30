2935b92556f1ce303482d7d4173f82d8db922239
jarek <jaroslaw.sokolik@clicktrans.pl>
2021-11-30 15:52:48 +0100

    day 1 lesson 2 and 3

--- Day_1/2.input_function.py
+++ Day_1/2.input_function.py
@@ -1,3 +1,4 @@
 # Input will get user input and save it in the variable
 name = input("What is your name?\n")
 print("Hi " + name)
+print(len(name))
---
+++ Day_1/3.variables.py
@@ -0,0 +1,25 @@
+# 🚨 Don't change the code below 👇 We want to switch print so the code looks like this:
+# input for a: 6
+# input for b: 9
+# print has to be opposite like this:
+# a: 9
+# b: 6
+a = input("a: ")
+b = input("b: ")
+# 🚨 Don't change the code above 👆
+
+####################################
+#Write your code below this line 👇
+
+c = a
+d = b
+b = c
+a = d
+
+
+#Write your code above this line 👆
+####################################
+
+# 🚨 Don't change the code below 👇
+print("a: " + a)
+print("b: " + b)
