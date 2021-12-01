efb7e8451e8dee307bb862870478fe289a93d42e
Jarosław Sokolik <jaroslaw.sokolik@clicktrans.pl>
2021-12-01 19:40:34 +0100

    Day 2 lesson 1 and 2

---
+++ Day_2/1.data_types.py
@@ -0,0 +1,21 @@
+#Data tyoes
+
+#String
+print("Hello")
+print("Test"[2])
+print("123456"[1:3])
+
+#Integer
+
+print(123 + 124)
+print(123_345_678)
+
+#Float
+
+print(1.22)
+print(334.33)
+
+#Boolean
+
+print(True)
+
---
+++ Day_2/2.type_conversion.py
@@ -0,0 +1,13 @@
+#To check the type of data
+
+print(type(22))
+print(str(22) + " number")
+
+# Write a program that adds the digits in a 2 digit number.
+# e.g. if the input was 35, then the output should be 3 + 5 = 8
+
+two_digit_number = input("Type a two digit number: ")
+first_number = int(two_digit_number[0])
+second_number = int(two_digit_number[1])
+print(first_number + second_number)
+
